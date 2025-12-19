3ab8152a7fac2fcdffea2b922ca8fed5ff961517
korenkox <korenkox@gmail.com>
2025-12-19 18:44:56 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,33 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'blob': 'blob 7s infinite',
+        'spin-slow': 'spin 20s linear infinite',
+        'reverse-spin': 'reverse-spin 25s linear infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+          '100%': { transform: 'translate(0px, 0px) scale(1)' },
+        },
+        'reverse-spin': {
+          from: { transform: 'rotate(360deg)' },
+          to: { transform: 'rotate(0deg)' },
+        }
+      },
+    },
   },
   plugins: [],
 }
